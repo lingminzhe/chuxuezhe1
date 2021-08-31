ef8a6e2d1df6abb79a284ea3066253ab0eaf97fb
mcyang <grg8724>
2021-08-31 16:23:46 +0800

    重构iam模块

--- grg-cloud-counter-common/grg-cloud-counter-common-log/src/main/java/com/grgbanking/counter/common/log/util/SysLogUtils.java
+++ grg-cloud-counter-common/grg-cloud-counter-common-log/src/main/java/com/grgbanking/counter/common/log/util/SysLogUtils.java
@@ -1,23 +1,75 @@
 package com.grgbanking.counter.common.log.util;
 
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.extra.servlet.ServletUtil;
+import cn.hutool.http.HttpUtil;
+import com.grgbanking.counter.iam.api.dto.SysLogDTO;
 import lombok.experimental.UtilityClass;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
  * 系统日志工具类
  *
- * @author grgbanking
+ * @author L.cm
  */
 @UtilityClass
 public class SysLogUtils {
 
+	public SysLogDTO getSysLog() {
+		HttpServletRequest request = ((ServletRequestAttributes) Objects
+				.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+		SysLogDTO sysLog = new SysLogDTO();
+		sysLog.setCreateBy(Objects.requireNonNull(getUsername()));
+		sysLog.setType(LogTypeEnum.NORMAL.getType());
+		sysLog.setRemoteAddr(ServletUtil.getClientIP(request));
+		sysLog.setRequestUri(URLUtil.getPath(request.getRequestURI()));
+		sysLog.setMethod(request.getMethod());
+		sysLog.setUserAgent(request.getHeader("user-agent"));
+		sysLog.setParams(HttpUtil.toParams(request.getParameterMap()).replace("%5B","").replace("%5D",""));
+		sysLog.setServiceId(getClientId());
+		return sysLog;
+	}
+
+	public SysLogDTO getSysLog(HttpServletRequest request, String username) {
+		SysLogDTO sysLog = new SysLogDTO();
+		sysLog.setCreateBy(username);
+		sysLog.setType(LogTypeEnum.NORMAL.getType());
+		sysLog.setRemoteAddr(ServletUtil.getClientIP(request));
+		sysLog.setRequestUri(URLUtil.getPath(request.getRequestURI()));
+		sysLog.setMethod(request.getMethod());
+		sysLog.setUserAgent(request.getHeader("user-agent"));
+		sysLog.setParams(HttpUtil.toParams(request.getParameterMap()));
+		sysLog.setServiceId(getClientId());
+		return sysLog;
+	}
+
+	/**
+	 * 获取客户端
+	 * @return clientId
+	 */
+	private String getClientId() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication instanceof OAuth2Authentication) {
+			OAuth2Authentication auth2Authentication = (OAuth2Authentication) authentication;
+			return auth2Authentication.getOAuth2Request().getClientId();
+		}
+		JdbcClientDetailsService d;
+		return null;
+	}
 
 	/**
 	 * 获取用户名称
 	 * @return username
 	 */
-	public String getUsername() {
+	private String getUsername() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (authentication == null) {
 			return null;
