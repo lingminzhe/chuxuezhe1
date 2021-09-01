7562dd943a4e8919495cfc05e5717e499206592b
mcyang <grg8724>
2021-09-01 13:19:03 +0800

    新增文件服务模块

---
+++ grg-cloud-counter-oss/grg-cloud-counter-oss-service/src/main/java/com/grgbanking/counter/oss/AppApplication.java
@@ -0,0 +1,19 @@
+package com.grgbanking.counter.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ */
+//@EnableGrgResourceServer
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
