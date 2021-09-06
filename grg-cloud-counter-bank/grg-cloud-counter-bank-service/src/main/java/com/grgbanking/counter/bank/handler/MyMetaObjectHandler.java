3224c07c4f61e8cefe01ae2a87be1f122cbcdb94
yekaitao <906931300@qq.com>
2021-09-06 16:20:49 +0800

    自动填充update和create date

---
+++ grg-cloud-counter-bank/grg-cloud-counter-bank-service/src/main/java/com/grgbanking/counter/bank/handler/MyMetaObjectHandler.java
@@ -0,0 +1,28 @@
+package com.grgbanking.counter.bank.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author: Ye Kaitao
+ * @create: 2021-09-06
+ * 需要注册到Spring容器中
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createDate", new Date(), metaObject);
+        this.setFieldValByName("updateDate", new Date(), metaObject);
+    }
+
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateDate", new Date(), metaObject);
+    }
+}
