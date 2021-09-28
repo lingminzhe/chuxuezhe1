8c090e7529ea27cd819c78de303f45184bd7e7d8
zhangyunzhe <zhangyunzhe@grgbanking.com>
2021-09-28 10:35:28 +0800

    device模块接入socket1

---
+++ grg-cloud-counter-device/grg-cloud-counter-device-service/src/main/java/com/grgbanking/counter/device/redis/handler/DefaultRedisBroadcastHandler.java
@@ -0,0 +1,38 @@
+package com.grgbanking.counter.device.redis.handler;
+
+import com.grgbanking.counter.common.core.util.SocketParam;
+import com.grgbanking.counter.common.socket.broadcast.constant.RedisBroadcastConstants;
+import com.grgbanking.counter.common.socket.broadcast.service.RedisBroadcastService;
+import com.grgbanking.counter.common.socket.socket.constant.SocketApiNoConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 无消息处理器处理的消息将在本类接收,默认情况转发给csr服务
+ */
+@Slf4j
+@Service
+public class DefaultRedisBroadcastHandler extends RedisBroadcastAbstractHandler {
+
+    @Autowired
+    private RedisBroadcastService redisBroadcastService;
+
+    @Override
+    public String setApiNo() {
+        return SocketApiNoConstants.DEFAULT_HANDLER_NAME;
+    }
+
+    /**
+     * 默认广播接收处
+     *
+     * @param param 消息内容
+     * @return
+     */
+    @Override
+    public void onMessage(String channel, SocketParam param) {
+        /**给CSR服务转发广播消息，CSR服务务必有处理该广播的处理器，否则CSR服务又会把消息转发回来*/
+        log.info("没有处理广播消息的handler，此处是默认接收消息：{}", param);
+        redisBroadcastService.sendBroadcast(RedisBroadcastConstants.BROADCAST_CHANNEL_CSR,param);
+    }
+}
---
+++ grg-cloud-counter-device/grg-cloud-counter-device-service/src/main/java/com/grgbanking/counter/device/redis/handler/RedisBroadcastConnectStatusChangeHandler.java
@@ -0,0 +1,31 @@
+package com.grgbanking.counter.device.redis.handler;
+
+import com.grgbanking.counter.common.core.util.SocketParam;
+import com.grgbanking.counter.common.socket.socket.constant.SocketApiNoConstants;
+import com.grgbanking.counter.common.socket.socket.service.SocketAbstractService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 连接状态发生变化的handler
+ */
+@Slf4j
+@Service
+public class RedisBroadcastConnectStatusChangeHandler extends RedisBroadcastAbstractHandler {
+
+    @Autowired
+    private SocketAbstractService socketService;
+
+    @Override
+    public String setApiNo() {
+        return SocketApiNoConstants.CONNECTION_STATUS_CHANGE;
+    }
+
+    @Override
+    public void onMessage(String channel, SocketParam param) {
+        /**给该用户发送对方的连接状态的变更通知，body里直接是终端id，所以直接取出来用*/
+        socketService.sendMessage((String)param.getBody(),param);
+    }
+
+}
