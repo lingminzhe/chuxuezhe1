56c1ca623711526fb5667f0240ec1424d695a007
zhangyunzhe <zhangyunzhe@grgbanking.com>
2021-09-28 09:47:35 +0800

    socket配置类注入问题

--- grg-cloud-counter-common/grg-cloud-counter-common-socket/src/main/java/com/grgbanking/counter/common/socket/socket/config/GrgSocketConfig.java
+++ grg-cloud-counter-common/grg-cloud-counter-common-socket/src/main/java/com/grgbanking/counter/common/socket/socket/config/GrgSocketConfig.java
@@ -2,13 +2,10 @@ package com.grgbanking.counter.common.socket.socket.config;
 
 import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -17,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
+@ComponentScan({"com.grgbanking.counter.common.socket.socket.config"})
 public class GrgSocketConfig {
 
 	@Autowired
