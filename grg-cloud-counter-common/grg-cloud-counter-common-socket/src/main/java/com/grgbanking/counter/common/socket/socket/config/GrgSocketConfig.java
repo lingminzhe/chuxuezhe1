b216d8f6db3effdbd696e076635684e194b7034d
mcyang <grg8724>
2021-09-28 11:20:39 +0800

    修复配置读取不到的问题

--- grg-cloud-counter-common/grg-cloud-counter-common-socket/src/main/java/com/grgbanking/counter/common/socket/socket/config/GrgSocketConfig.java
+++ grg-cloud-counter-common/grg-cloud-counter-common-socket/src/main/java/com/grgbanking/counter/common/socket/socket/config/GrgSocketConfig.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
-@ComponentScan({"com.grgbanking.counter.common.socket.socket.config"})
 public class GrgSocketConfig {
 
 	@Autowired
