17a2a8afe9a95e4cdd55c6e0bcc029a1e3d97728
yekaitao <906931300@qq.com>
2021-10-16 15:51:26 +0800

    Dto

---
+++ grg-cloud-counter-app/grg-cloud-counter-app-service/src/main/java/com/grgbanking/counter/app/dto/FileInfoDto.java
@@ -0,0 +1,16 @@
+package com.grgbanking.counter.app.dto;
+
+import lombok.Data;
+
+/**
+ * @author: Ye Kaitao
+ * @create: 2021-10-16
+ */
+@Data
+public class FileInfoDto {
+
+    private String file;
+    private String fileBusiType;
+
+
+}
