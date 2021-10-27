0f6f045af7b96f42f031fcd320d955c82cf21e9f
zhangyunzhe <zhangyunzhe@grgbanking.com>
2021-10-27 10:13:29 +0800

    csr发送短信推送手机号码

--- grg-cloud-counter-csr/grg-cloud-counter-csr-service/src/main/java/com/grgbanking/counter/csr/controller/MobileController.java
+++ grg-cloud-counter-csr/grg-cloud-counter-csr-service/src/main/java/com/grgbanking/counter/csr/controller/MobileController.java
@@ -3,7 +3,6 @@ package com.grgbanking.counter.csr.controller;
 import com.grgbanking.counter.bank.api.dubbo.RemoteMobileService;
 import com.grgbanking.counter.common.core.util.Resp;
 import com.grgbanking.counter.common.core.util.SocketParam;
-import com.grgbanking.counter.common.core.util.SocketParamHead;
 import com.grgbanking.counter.common.socket.broadcast.constant.RedisBroadcastConstants;
 import com.grgbanking.counter.common.socket.broadcast.service.RedisBroadcastService;
 import com.grgbanking.counter.common.socket.lineup.service.LineupService;
@@ -11,7 +10,6 @@ import com.grgbanking.counter.iam.api.dubbo.RemoteUserService;
 import com.grgbanking.counter.iam.api.entity.SysUserEntity;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 
 /**
  * 手机验证码
@@ -47,11 +46,12 @@ public class MobileController {
 	public Resp sendSmsCode(@PathVariable String mobile, HttpServletRequest request) {
 		boolean b = mobileService.sendSmsCode(mobile);
 		if (b){
+			HashMap<Object, Object> map = new HashMap<>(1);
 			SysUserEntity authorization = remoteUserService.currentUser(request.getHeader("Authorization"));
 			String customerId = lineupService.findCustomer(String.valueOf(authorization.getUserId()));
-			SocketParamHead head = SocketParamHead.success("verifySms");
-			head.setClientId(customerId);
-			broadcastService.sendBroadcast(RedisBroadcastConstants.BROADCAST_CHANNEL_APP, SocketParam.success(head));
+			map.put("phone",mobile);
+			SocketParam param = lineupService.successParam("customerId", "verifySms", "100001", map);
+			broadcastService.sendBroadcast(RedisBroadcastConstants.BROADCAST_CHANNEL_APP, param);
 			return Resp.success("短信发送成功");
 		}else {
 			return Resp.failed("短信发送失败");
